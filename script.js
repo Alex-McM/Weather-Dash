d1c90b56e775faefbd95c24272347adb6b5abf47
El Psy Congroo <mcmasterjalexander@gmail.com>
2021-01-04 17:52:49 +1100

    AJAX call created

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function find(c){
 }
 
 var APIKey="ebc3e395f0cf7db283b9984d25a04632";
-
 function displayWeather(event){
     event.preventDefault();
     if(searchCity.val().trim()!==""){
@@ -28,3 +27,26 @@ function displayWeather(event){
         currentWeather(city);
     }
 }
+
+function currentWeather(city){
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+   
+   
+    var weathericon= response.weather[0].icon;
+    var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+   
+    var date=new Date(response.dt*1000).toLocaleDateString();
+   
+$(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+  
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+$(currentHumidty).html(response.main.humidity+"%");
+    
+    var ws=response.wind.speed;
+    var windsmph=(ws*2.237).toFixed(1);
+$(currentWSpeed).html(windsmph+"MPH");
